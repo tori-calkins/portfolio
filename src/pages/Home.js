9858df8aba8eb5454c7e657fbbb95988597d9dbb
tori-calkins <tori.calkins2705@gmail.com>
2023-11-16 16:55:28 -0800

    got profile pic on home page

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import profilepic from '../assets/profilepicupdated.jpg'
+
+const Home = () => {
+    return (
+        <>
+            <img classname="profilepicture" src={profilepic} alt="Profile Picture of me"/>
+        </>
+    )
+}
+
+export default Home
