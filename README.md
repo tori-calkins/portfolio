1a1dfb2c6391db4b66852813118648bdb9b58a6c
tori-calkins <tori.calkins2705@gmail.com>
2023-11-21 14:13:19 -0800

    fixed icons at bottom

--- README.md
+++ README.md
@@ -8,10 +8,10 @@
 
     - have the learn more about me button take people to the pdf version of my resume ✅
 
-    - ask ilene if zoomedin profile pic is better or regular one
+    - ask ilene if zoomedin profile pic is better or regular one✅
 
     - have a form somewhere to send an email to get in contact w me 
 
-    - figure out how to get rid of the underline on my learn more about me button
+    - figure out how to get rid of the underline on my learn more about me button ✅
 
     - 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import profilepic from '../assets/profilepicupdated.jpg'
-// import profilePic from '../assets/ProfilePicZoomedIn.png'
-import tealwood from '../assets/tealwood.png'
+import tealwood from '../assets/teal_wood_background.jpeg'
 import { FaLinkedin } from "react-icons/fa";
 import { TbBrandJavascript } from "react-icons/tb";
 import { DiRuby } from "react-icons/di";
@@ -12,7 +11,8 @@ import { FaSlack } from "react-icons/fa";
 import { IoLogoGithub } from "react-icons/io";
 import { TbBrandVscode } from "react-icons/tb";
 import { PiFileHtmlLight } from "react-icons/pi";
-import { Link } from "react-router-dom"
+import { NavLink, Link } from "react-router-dom"
+
 
 const Home = () => {
 
@@ -22,12 +22,12 @@ const Home = () => {
                 <img id="profilepicture" src={profilepic} alt="Profile Picture of me"/>
                 <h1 className="intro">Hi, I'm Tori!</h1>
                 <p className="intro-p" >I'm a junior full-stack developer based in San Diego, California. I've worked on web development projects using HTML, CSS, JavaScript, React.js, Ruby, and Ruby on Rails, and my passion for creative problem-solving drives my constant skill growth.</p>
-                <div>
-                    <Link to="https://docs.google.com/document/d/1fNNWa324GOWl8CfxzxyMXu4IaLdDclG32TQjGIxsb0w/edit" target="_blank">
-                        <button id="aboutMeButton">
+                <div id="aboutMeButton">
+                    <NavLink to="https://docs.google.com/document/d/1fNNWa324GOWl8CfxzxyMXu4IaLdDclG32TQjGIxsb0w/edit" target="_blank">
+                        <button id="aboutMeButtonColor">
                             Learn More About Me
                         </button>
-                    </Link>
+                    </NavLink>
                 </div>
                 <img id="containerImage" src={tealwood} alt="teal wood slabs"/>
                 <div id="icons"> 
@@ -44,8 +44,12 @@ const Home = () => {
                 </div>
                 <img id="containerImage" src={tealwood} alt="teal wood slabs"/>
                 <div id="icons-2">
-                <FaLinkedin />
-                <IoLogoGithub />
+                    <NavLink to="https://www.linkedin.com/in/victoria-calkins-/" target="_blank">
+                        <FaLinkedin id="icons-3"/>
+                    </NavLink>
+                    <NavLink to="https://github.com/tori-calkins" target="_blank">
+                        <IoLogoGithub id="icons-4"/>
+                    </NavLink>
                 </div>
             </body>
         </>
